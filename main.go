e478e4b79563b9ac743862bc05a0ff5b2cab3fad
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-09-23 16:02:44 +0700

    custom handlers

--- main.go
+++ main.go
@@ -7,12 +7,19 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("./")))
 
-	s := &http.Server{
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./"))))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	svr := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	log.Fatal(s.ListenAndServe())
+	log.Fatal(svr.ListenAndServe())
 }
