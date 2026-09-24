f85ae71f5753b78bf5ba035de9281f28223c8237
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-09-24 09:13:14 +0700

    implement simple middlware

--- main.go
+++ main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getFileserverHits(w http.ResponseWriter, r *http.Request) {
+	currentHits := cfg.fileserverHits.Load()
+
+	w.Write([]byte(fmt.Sprintf("Hits: %d", int32(currentHits))))
+}
+
+func (cfg *apiConfig) resetFileserverHits(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./"))))
+	cfg := &apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("/metrics", cfg.getFileserverHits)
+	mux.HandleFunc("/reset", cfg.resetFileserverHits)
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
