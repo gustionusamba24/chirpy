cd41060b4161b84b806dc8a51bc42e213d115856
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-09-25 15:33:56 +0700

    implement a graceful shutdown in order to make it save when it has uncompleted request

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"sync/atomic"
+	"syscall"
+	"time"
 )
 
 type apiConfig struct {
@@ -34,20 +40,54 @@ func main() {
 	cfg := &apiConfig{
 		fileserverHits: atomic.Int32{},
 	}
-	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+
+	appRoot := "/app/"
+
+	mux.Handle(appRoot, cfg.middlewareMetricsInc(http.StripPrefix(appRoot, http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("GET /metrics", cfg.getFileserverHits)
 	mux.HandleFunc("POST /reset", cfg.resetFileserverHits)
 
 	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(60 * time.Second)
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
 
+	port := ":8080"
+
 	svr := &http.Server{
-		Addr:    ":8080",
+		Addr:    port,
 		Handler: mux,
 	}
 
-	log.Fatal(svr.ListenAndServe())
+	go func() {
+		log.Printf("Server started and listening on %s\n", port)
+
+		// ListenAndServe always returns a non-nil error
+		// after Shutdown or Close, the returned error is http.ErrServerClosed
+		// program execution will stop at that line
+		// the subsequent code will not be executed until the server is shutdown
+		err := svr.ListenAndServe()
+
+		if !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("HTTP Server error: %v", err)
+		}
+
+		log.Println("Stopped serving new connections")
+	}()
+
+	// main program will wait here until it receives a signal to shutdown
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownRelease()
+
+	if err := svr.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("HTTP shutdown error: %v", err)
+	}
+
+	log.Println("Graceful shutdown completed")
 }
